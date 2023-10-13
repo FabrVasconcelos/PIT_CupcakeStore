2e93c74912ff039a16a10512a9fb61ccac9ad29a
Fabrício Vasconcelos <79899@pmenosmfa.com.br>
2023-10-12 22:52:09 -0300

    Inclusão de dev

--- CupCakeStore/main.py
+++ CupCakeStore/main.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/usuarios/<nome_usuario>")
-def usuarios(nome_usuario):
-    return render_template("usuarios.html", nome_usuario=nome_usuario)
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
+
+### Temporariamente desativado a main devido a MVP
+#@app.route("/usuarios/<nome_usuario>")
+#def usuarios(nome_usuario):
+#    return render_template("usuarios.html", nome_usuario=nome_usuario)
 
 
 if __name__ == "__main__":
