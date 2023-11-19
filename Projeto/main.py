8b9bed38191ccf95e323b60e4fad1c351c4ac2eb
Fabrício Vasconcelos <79899@pmenosmfa.com.br>
2023-11-19 16:00:21 -0300

    Correção de BUG's e CSS

--- Projeto/main.py
+++ Projeto/main.py
@@ -18,11 +18,5 @@ def contato():
 def obrigado():
     return render_template("obrigado.html")
 
-### Temporariamente desativado a main devido a MVP
-#@app.route("/usuarios/<nome_usuario>")
-#def usuarios(nome_usuario):
-#    return render_template("usuarios.html", nome_usuario=nome_usuario)
-
-
 if __name__ == "__main__":
     app.run(debug=True)
